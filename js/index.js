24cd5b0a2cb842c3225a0c827d04f0217c4f713f
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-04-03 01:24:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -19,6 +19,9 @@ function getCommits(el) {
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
   req.open('GET', 'https://api.github.com/repos/' + name + '/commits');
+  console.log(name);
+  console.log("Hello");
+  console.log(el);
   req.send();
 }
 
